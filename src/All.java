74e827da80aa5b5507c9c6f1f5544eece080ac88
ksks723 <ksks723@gmail.com>
2020-09-29 18:00:16 +0900

    그냥 스캐너

--- src/All.java
+++ src/All.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class All {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("입력하세요");
+        String why = sc.nextLine();
+        System.out.println(why);
     }
 }
