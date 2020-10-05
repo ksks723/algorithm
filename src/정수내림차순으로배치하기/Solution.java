19e8c2acac9340ace81025cf532465345bbf3232
ksks723 <ksks723@gmail.com>
2020-10-05 13:43:24 +0900

    버블정렬 sort없이

--- src/정수내림차순으로배치하기/Solution.java
+++ src/정수내림차순으로배치하기/Solution.java
@@ -2,27 +2,33 @@ package 정수내림차순으로배치하기;
 
 public class Solution {
     public static void main(String[] args) {
-        long anwer = 0;
-        long n = 118637;
-
-        String nn = Long.toString(n);
-        int[] arr = new int[nn.length()];
-        for(int i = 0; i<nn.length();i++){
-            arr[i] = nn.charAt(i);
-
-        }
-
+        int[] arr = new int[]{25,65,33,27,85,33,1,3};
+        int size = arr.length;
 
         //버블정렬 sort안쓰고 해보기
-        for(int i = 0 ; i< arr.length ; i++){
-            for(int j = 0; j<arr.length; j++ ){
-                if(arr[i]>arr[j]){
-
+        for(int i=0; i < size-1; i++) {
+            System.out.printf("\n버블 정렬 %d 단계 : ", i + 1);
+            for(int j=0; j < size -1 -i; j++) {
+                if(arr[j] > arr[j+1])
+                    swap(arr, j, j+1);
+                System.out.printf("\n");
+                for(int k=0; k < size; k++) {
+                    System.out.printf("%4d", arr[k]);
                 }
-
             }
+
         }
 
 
+
+
+
+
+
+    }
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
