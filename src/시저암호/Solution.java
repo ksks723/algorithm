8f1f6e4bde2b5936a6e76166c1307aace98f0995
ksks723 <ksks723@gmail.com>
2020-10-08 00:13:16 +0900

    시저암호 정확성높이는중

--- src/시저암호/Solution.java
+++ src/시저암호/Solution.java
@@ -2,19 +2,27 @@ package 시저암호;
 
 public class Solution {
     public static void main(String[] args) {
-        String s = "a Z z";
+        String s = "a Z z A";
         int n = 7;
         char[] cc = s.toCharArray();
         for (int i = 0; i < cc.length; i++) {
-            if ((int) (cc[i]) == 32) {
+            int temp = (int) (cc[i]) ;
+            if (temp == 32) {
                 continue;
-            } else {
-                int temp = (int) (cc[i]) + n;
-                if(temp>90 && temp<97 || temp>122){
-                    temp-=26;
-                }//이쯤에서 뭘 어떻게 바꾸면 될거같은디
-                cc[i] = (char) temp;
+            }else if(65<=temp && temp <=90 ){
+                if(temp+n<=90){
+                    cc[i] = (char)(temp+n);
+                }else{
+                    cc[i] = (char)(temp+n-26);
+                }
+            } else if(97<=temp && temp <=122){
+                if(temp+n<=122){
+                    cc[i] = (char)(temp+n);
+                }else{
+                    cc[i] = (char)(temp+n-26);
+                }
             }
+
         }
         String answer = new String(cc);
         System.out.println(cc);
