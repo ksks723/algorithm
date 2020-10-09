70f8898b500a95a3040d4e919b65e70fb686163c
ksks723 <ksks723@gmail.com>
2020-10-09 16:12:07 +0900

    문자열내맘대로정렬하기

---
+++ src/문자열내마음대로정렬하기/Solution.java
@@ -0,0 +1,19 @@
+package 문자열내마음대로정렬하기;
+
+import java.util.Arrays;
+
+public class Solution {
+    public String[] solution(String[] strings, int n){
+        String[] answer = new String[strings.length];
+
+        return answer;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] answer = s.solution(new String[]{"sun","bed","car"},1);
+        System.out.println(Arrays.toString(answer));
+    }
+}
