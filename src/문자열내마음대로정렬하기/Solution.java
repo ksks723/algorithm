f63b3376815c92e655569d1d7e0693ef137c90f1
ksks723 <ksks723@gmail.com>
2020-10-09 17:14:00 +0900

    문자열내맘대로정렬하기 끝

--- src/문자열내마음대로정렬하기/Solution.java
+++ src/문자열내마음대로정렬하기/Solution.java
@@ -1,18 +1,63 @@
 package 문자열내마음대로정렬하기;
 
-import java.util.Arrays;
 
-public class Solution {
-    public String[] solution(String[] strings, int n){
-        String[] answer = new String[strings.length];
+import java.util.*;
 
+class Solution { //다른사람풀이임
+    public String[] solution(String[] strings, int n) {
+
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2){
+                char c1 = s1.charAt(n);
+                char c2 = s2.charAt(n);
+
+                // n번째 문자가 같을 경우에 원본 스트링을 사전순으로
+                if(c1 == c2){
+                    return s1.compareTo(s2);
+                } else return c1 - c2;
+            }
+        });
+
+        return strings;
+    }
+}
+
+class Solution2 { //내가하려던거랑비슷한거
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < strings.length; i++) {
+            arr.add("" + strings[i].charAt(n) + strings[i]);
+        }
+        Collections.sort(arr);
+        answer = new String[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i).substring(1, arr.get(i).length());
+        }
         return answer;
+    }
+}
+
+class Solution3{
+    public String[] solution(String[] strings, int n){
+        String[] answer = {};
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i=0; i<strings.length; i++){
+            arr.add(""+strings[i].charAt(n)+strings[i]);
+        }
+        Collections.sort(arr);
+        answer = new String[arr.size()];
+        for(int i = 0; i<arr.size();i++){
+            answer[i] = arr.get(i).substring(1);
+        }
+        return  answer;
     }
 }
 
 class Main{
     public static void main(String[] args) {
-        Solution s = new Solution();
+        Solution3 s = new Solution3();
         String[] answer = s.solution(new String[]{"sun","bed","car"},1);
         System.out.println(Arrays.toString(answer));
     }
