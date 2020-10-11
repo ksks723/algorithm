b9cf71c1afda9ac354e05b11c34bb5f5c32db027
ksks723 <ksks723@gmail.com>
2020-10-11 19:23:22 +0900

    콜라츠추측 완료

--- src/콜라츠추측/Solution.java
+++ src/콜라츠추측/Solution.java
@@ -1,17 +1,25 @@
 package 콜라츠추측;
 
-public class Solution {
+class Solution {
+    int answer=0;
     public int solution(int num) {
-        int answer = 0;
-        do{
-            if(num%2==0){
-
-            }
-        }while (num ==1);
-
-        return answer;
+        if(num == 1){
+            return answer;
+        }
+        if(answer==500){
+            return -1;
+        }
+        answer++;
+        if (num % 2 == 1) {
+            // 홀수
+            return solution(num * 3 + 1);
+        } else{
+            // 짝수
+            return solution(num / 2);
+        }
     }
 }
+
 
 class Main{
     public static void main(String[] args) {
