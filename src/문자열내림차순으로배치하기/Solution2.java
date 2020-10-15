ff4c07490d951f7e9766f622ea9e48dbdba965b8
ksks723 <ksks723@gmail.com>
2020-10-15 18:44:42 +0900

    문자열내림차순으로배치하기 버블정렬 제대로

--- src/문자열내림차순으로배치하기/Solution2.java
+++ src/문자열내림차순으로배치하기/Solution2.java
@@ -2,19 +2,16 @@ package 문자열내림차순으로배치하기;
 
 import java.util.Arrays;
 
-public class Solution2 { //런타임 초과
-    public String solution(String s){  //StringBuilder 을 사용
-
-        char[] ch = s.toCharArray();
-        Arrays.sort(ch);
-        return new StringBuilder(new String(ch)).reverse().toString();
+public class Solution2 {
+    public String Solution2(String str){
+        char[] sol = str.toCharArray();
+        Arrays.sort(sol);
+        return new StringBuilder(new String(sol)).reverse().toString();
     }
-}
 
-class Main2{
+    // 아래는 테스트로 출력해 보기 위한 코드입니다.
     public static void main(String[] args) {
-        Solution2 s = new Solution2();
-        String answer = s.solution("Zbcdefg");
-        System.out.println(answer);
+        Solution2 rs = new Solution2();
+        System.out.println( rs.Solution2("Zbcdefg") );
     }
 }
