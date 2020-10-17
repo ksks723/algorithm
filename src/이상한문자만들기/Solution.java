21855b3af873844ff0298a109f6979bf2a4bd532
ksks723 <ksks723@gmail.com>
2020-10-17 19:30:54 +0900

    이상한 문자 만들기 진행중

--- src/이상한문자만들기/Solution.java
+++ src/이상한문자만들기/Solution.java
@@ -3,6 +3,15 @@ package 이상한문자만들기;
 public class Solution {
     public String solution(String s){
         String answer = "";
+        String[] arr = s.split(" ");
+        for(int i = 0; i<arr.length;i++){
+            for(int j=0; j<arr[i].length();j++){
+                if(arr[i].charAt(j)%2==0){
+                    char c = Character.toUpperCase(arr[i].charAt(j));
+                    System.out.println(c);
+                }
+            }
+        }
         return answer;
     }
 
