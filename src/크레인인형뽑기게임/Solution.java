ec0ff9ec6afc5c345cd4c5f884be235116c6ce82
ksks723 <ksks723@gmail.com>
2020-10-22 19:00:37 +0900

    크레인시작

--- src/크레인인형뽑기게임/Solution.java
+++ src/크레인인형뽑기게임/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int anwer = s.solution(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}}, new int[]{1, 5, 3, 5, 1, 2, 1, 4});
-        System.out.println(anwer);
+        int answer = s.solution(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}}, new int[]{1, 5, 3, 5, 1, 2, 1, 4});
+        System.out.println(answer);
     }
 }
